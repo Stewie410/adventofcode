d1e8a5ab7035513a2036a2be17da06a12a0acdad
Alex Paarfus <stewie410@gmail.com>
2024-01-03 16:55:25 -0500

    AOC 2023 days 1-4 -- I'm pretty far behind, I'd say

---
+++ 2023/bash/02.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+solution() {
+    local game sum power r g b idx color value
+
+    while read -r game; do
+        (( r = 0, g = 0, b = 0, idx++ ))
+
+        while read -r value color; do
+            case "${color,,}" in
+                red )   (( r < value )) && r="${value}";;
+                green ) (( g < value )) && g="${value}";;
+                blue )  (( b < value )) && b="${value}";;
+            esac
+        done < <(tr "," '\n' <<< "${game}")
+
+        if (( r <= 12 && g <= 13 && b <= 14 )); then
+            (( sum += idx ))
+        fi
+
+        (( power += r * g * b ))
+    done < <(sed '/^\s*$/d;s/^.*: //;s/[,;] /,/g' "${1}")
+
+    printf '%d\n' "${sum}" "${power}"
+}
---
+++ 2023/bash/03.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+solution() {
+    local -A candidates
+    local -a map
+    local x y px py width height parts ratios
+
+    mapfile -t map < <(fold --width="1" "${1}")
+    width="$(wc --max-line-length < "${1}")"
+    height="$(wc --lines < "${1}")"
+
+    for (( y = 0; y < height; y++ )); do
+        for (( x = 0; x < width; x++ )); do
+            [[ "${map[y * width + x]}" =~ [0-9] ]] || continue
+
+            unset num
+            while (( x < width )) && [[ "${map[y * width + x]}" =~ [0-9] ]]; do
+                num+="${map[y * width + x]}"
+                (( x++ ))
+            done
+
+            for (( py = y - 1; py <= y + 1; py++ )); do
+                for (( px = x - ${#num} - 1; px <= x; px++ )); do
+                    if [[ "${map[py * width + px]:-0}" =~ [^0-9\.] ]]; then
+                        (( parts += num ))
+                        [[ "${map[py * width + px]:-0}" == '*' ]] && \
+                            candidates["${py},${px}"]+="${num},"
+                        break 2
+                    fi
+                done
+            done
+        done
+    done
+
+    for x in "${candidates[@]}"; do
+        mapfile -t map < <(tr "," '\n' <<< "${x%,*}")
+        (( ${#map[@]} == 2 )) && (( ratios += map[0] * map[1] ))
+    done
+
+    printf '%d\n' "${parts}" "${ratios}"
+}
