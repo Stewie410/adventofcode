ae7c25361a1b460b6f0862bfe2dd9da5efbecd7a
Alex Paarfus <stewie410@gmail.com>
2024-12-01 14:40:40 -0500

    Added insertion & quicksort impl

--- 2024/bash/01.sh
+++ 2024/bash/01.sh
@@ -1,22 +1,66 @@
 #!/usr/bin/env bash
 
-# adapted from SO answer
-# https://stackoverflow.com/a/55615989
-bubble() {
-    local -n list="${1}"
-    local i j
+# https://stackoverflow.com/a/30576368
+quicksort() {
+    (( $# == 0 )) && return 0
 
-    for (( i = 0; i < ${#list[@]}; i++ )); do
-        for (( j = 0; j < ${#list[@]} - i - 1; j++ )); do
-            if (( list[j] > list[j+1] )); then
-                # Thanks Primeagen for that one short
-                # https://en.wikipedia.org/wiki/XOR_swap_algorithm
-                (( list[j] = list[j] ^ list[j+1] ))
-                (( list[j+1] = list[j+1] ^ list[j] ))
-                (( list[j] = list[j] ^ list[j+1] ))
-            fi
+    local -a smaller larger
+    local pivot i
+
+    smaller=()
+    larger=()
+    pivot="${1}"
+    shift
+
+    for i in "${@}"; do
+        if (( i < pivot )); then
+            smaller+=( "${i}" )
+        else
+            larger+=( "${i}" )
+        fi
+    done
+
+    mapfile -t smaller < <(quicksort "${smaller[@]}")
+    mapfile -t larger < <(quicksort "${larger[@]}")
+    printf '%s\n' "${smaller[@]}" "${pivot}" "${larger[@]}"
+}
+
+# https://www.java67.com/2014/09/insertion-sort-in-java-with-example.html
+insertion() {
+    local -a arr
+    local i j n
+
+    arr=( "${@}" )
+
+    for (( i = 1; i < $#; i++ )); do
+        j="${i}"
+        while (( j > 0 )) && (( arr[j] < arr[j-1] )); do
+            n="${arr[j]}"
+            (( arr[j] = arr[j-1], arr[j-1] = n ))
+            (( j-- ))
         done
     done
+
+    printf '%s\n' "${arr[@]}"
+}
+
+# https://stackoverflow.com/a/55615989
+bubble() {
+    local -a arr
+    local i j n flag
+    arr=( "${@}" )
+
+    for (( i = 0; i < $#; i++ )); do
+        unset flag
+        for (( j = 0; j < $# - i - 1; j++ )); do
+            (( arr[j] > arr[j+1] )) || continue
+            (( n = arr[j], arr[j] = arr[j+1], arr[j+1] = n ))
+            flag="1"
+        done
+        [[ -z "${flag}" ]] && break
+    done
+
+    printf '%s\n' "${arr[@]}"
 }
 
 solution() {
@@ -29,8 +73,14 @@ solution() {
         (( counts[r]++ ))
     done < "${1}"
 
-    bubble left
-    bubble right
+    # mapfile -t left < <(bubble "${left[@]}")
+    # mapfile -t right < <(bubble "${right[@]}")
+
+    # mapfile -t left < <(insertion "${left[@]}")
+    # mapfile -t right < <(insertion "${right[@]}")
+
+    mapfile -t left < <(quicksort "${left[@]}")
+    mapfile -t right < <(quicksort "${right[@]}")
 
     for (( i = 0; i < ${#left[@]}; i++ )); do
         dist="$(( left[i] - right[i] ))"
