18f16d84aee6aed90ce210f0155d2a8d4daf3450
Alex Paarfus <stewie410@gmail.com>
2024-12-02 19:54:08 -0500

    bash/2021/02 optimization

--- bash/2021/02.sh
+++ bash/2021/02.sh
@@ -1,37 +1,17 @@
 #!/usr/bin/env bash
 
-part_a() {
-    local direction amount x y
-
-    while read -r direction amount; do
-        case "${direction,,}" in
-            forward )   x="$(( x + amount ))";;
-            down )      y="$(( y + amount ))";;
-            up )        y="$(( y - amount ))";;
-        esac
-    done < "${1}"
-
-    printf '%s\n' "$(( x * y ))"
-}
-
-part_b() {
-    local direction amount x y a
-
-    while read -r direction amount; do
-        case "${direction,,}" in
-            forward )   x="$(( x + amount ))"; y="$(( y + ( a * amount ) ))";;
-            down )      a="$(( a + amount ))";;
-            up )        a="$(( a - amount ))";;
-        esac
-    done < "${1}"
-
-    printf '%s\n' "$(( x * y ))"
-}
-
 main() {
-    set -- "${1:-/dev/stdin}"
-    part_a "${1}"
-    part_b "${1}"
+    local direction amount x ay by z
+
+    while read -r direction amount; do
+        case "${direction,,}" in
+            forward )   (( x += amount, by += z * amount ));;
+            down )      (( ay += amount, z += amount ));;
+            up )        (( ay -= amount, z -= amount ));;
+        esac
+    done < "${1:-/dev/stdin}"
+
+    printf '%s\n' "$(( x * ay ))" "$(( x * by ))"
 }
 
 main "${@}"
