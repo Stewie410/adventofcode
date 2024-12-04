1dfbcd520cb3bf364035accaf1a06bce35ece500
Alex Paarfus <stewie410@gmail.com>
2024-12-04 10:40:29 -0500

    bash/2024/04: dry up part_a() a little

--- bash/2024/04.sh
+++ bash/2024/04.sh
@@ -11,50 +11,50 @@ part_a() {
         for (( x = 0; x < w; x++ )); do
             [[ "${lines[y]:x:1}" != "X" ]] && continue
 
-            # EAST
+            # east
             if (( x <= w - 3 )); then
+                # north-east
+                if (( y >= 3 )); then
+                    word="${lines[y-1]:x+1:1}${lines[y-2]:x+2:1}${lines[y-3]:x+3:1}"
+                    [[ "${word}" == "MAS" ]] && (( count++ ))
+                fi
+
+                # south-east
+                if (( y <= ${#lines[@]} - 3 )); then
+                    word="${lines[y+1]:x+1:1}${lines[y+2]:x+2:1}${lines[y+3]:x+3:1}"
+                    [[ "${word}" == "MAS" ]] && (( count++ ))
+                fi
+
                 [[ "${lines[y]:x:4}" == "XMAS" ]] && (( count++ ))
             fi
 
-            # WEST
+            # west
             if (( x >= 3 )); then
+                # north-west
+                if (( y >= 3 )); then
+                    word="${lines[y-1]:x-1:1}${lines[y-2]:x-2:1}${lines[y-3]:x-3:1}"
+                    [[ "${word}" == "MAS" ]] && (( count++ ))
+                fi
+
+                # south-west
+                if (( y <= ${#lines[@]} - 3 )); then
+                    word="${lines[y+1]:x-1:1}${lines[y+2]:x-2:1}${lines[y+3]:x-3:1}"
+                    [[ "${word}" == "MAS" ]] && (( count++ ))
+                fi
+
                 [[ "${lines[y]:x-3:4}" == "SAMX" ]] && (( count++ ))
             fi
 
-            # NORTH
+            # north
             if (( y >= 3 )); then
-                word="${lines[y]:x:1}${lines[y-1]:x:1}${lines[y-2]:x:1}${lines[y-3]:x:1}"
-                [[ "${word}" == "XMAS" ]] && (( count++ ))
+                word="${lines[y-1]:x:1}${lines[y-2]:x:1}${lines[y-3]:x:1}"
+                [[ "${word}" == "MAS" ]] && (( count++ ))
             fi
 
-            # SOUTH
+            # south
             if (( y <= ${#lines[@]} - 3 )); then
-                word="${lines[y]:x:1}${lines[y+1]:x:1}${lines[y+2]:x:1}${lines[y+3]:x:1}"
-                [[ "${word}" == "XMAS" ]] && (( count++ ))
-            fi
-
-            # NE
-            if (( x <= w - 3 && y >= 3 )); then
-                word="${lines[y]:x:1}${lines[y-1]:x+1:1}${lines[y-2]:x+2:1}${lines[y-3]:x+3:1}"
-                [[ "${word}" == "XMAS" ]] && (( count++ ))
-            fi
-
-            # NW
-            if (( x >= 3 && y >= 3 )); then
-                word="${lines[y]:x:1}${lines[y-1]:x-1:1}${lines[y-2]:x-2:1}${lines[y-3]:x-3:1}"
-                [[ "${word}" == "XMAS" ]] && (( count++ ))
-            fi
-
-            # SE
-            if (( y <= ${#lines[@]} - 3 && x <= w - 3 )); then
-                word="${lines[y]:x:1}${lines[y+1]:x+1:1}${lines[y+2]:x+2:1}${lines[y+3]:x+3:1}"
-                [[ "${word}" == "XMAS" ]] && (( count++ ))
-            fi
-
-            # SW
-            if (( y <= ${#lines[@]} - 3 && x >= 3 )); then
-                word="${lines[y]:x:1}${lines[y+1]:x-1:1}${lines[y+2]:x-2:1}${lines[y+3]:x-3:1}"
-                [[ "${word}" == "XMAS" ]] && (( count++ ))
+                word="${lines[y+1]:x:1}${lines[y+2]:x:1}${lines[y+3]:x:1}"
+                [[ "${word}" == "MAS" ]] && (( count++ ))
             fi
         done
     done
