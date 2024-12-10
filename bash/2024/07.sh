150a52cd8c2b9f6e8f6f43bbc06ea513c91f5d24
Alex Paarfus <stewie410@gmail.com>
2024-12-10 10:10:46 -0500

    bash/2024/07: recursive solution -- its quite slow, at 2min per run...

---
+++ bash/2024/07.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2178
+
+# target total index ref(nums) [includeConcat]
+check() {
+    local -n nums="${4}"
+
+    (( ${2} == ${1} && ${3} == ${#nums[@]} )) && return 0
+    (( ${2} >  ${1} || ${3} == ${#nums[@]} )) && return 1
+
+    check "${1}" "$(( ${2} + nums[${3}] ))" "$(( ${3} + 1 ))" "${@:4}" \
+        && return 0
+    check "${1}" "$(( ${2} * nums[${3}] ))" "$(( ${3} + 1 ))" "${@:4}" \
+        && return 0
+
+    (( ${5} == 1 )) \
+        && check "${1}" "${2}${nums[${3}]}" "$(( ${3} + 1 ))" "${@:4}" \
+        && return 0
+
+    return 1
+}
+
+main() {
+    local -a operands
+    local line p1
+
+    while read -r line; do
+        read -ra operands <<< "${line##*: }"
+        if check "${line%:*}" "${operands[0]}" "1" "operands" "0"; then
+            (( p1 += ${line%:*}, p2 += ${line%:*} ))
+        elif check "${line%:*}" "${operands[0]}" "1" "operands" "1"; then
+            (( p2 += ${line%:*} ))
+        fi
+    done < "${1:-/dev/stdin}"
+
+    printf '%s\n' "${p1}" "${p2}"
+}
+
+main "${@}"
