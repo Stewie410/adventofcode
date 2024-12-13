295e08d19b4a635d33fcc585e5335408b7dc223f
Alex Paarfus <stewie410@gmail.com>
2024-12-13 15:48:06 -0500

    Removed legacy blink() func; renamed iterative() for some spice

--- bash/2024/11.sh
+++ bash/2024/11.sh
@@ -1,26 +1,7 @@
 #!/usr/bin/env bash
 
-# ref(out) stone count
-blink() {
-    local -n out="${1}"
-    local stone count len
-    (( stone = ${2}, count = ${3} - 1 ))
-
-    if (( count < 0 )); then
-        (( out++ ))
-    elif (( stone == 0 )); then
-        blink "${1}" "1" "${count}"
-    elif (( ${#stone} % 2 == 0 )); then
-        (( len = ${#stone} / 2 ))
-        blink "${1}" "${stone:0:len}" "${count}"
-        blink "${1}" "$(( 10#${stone:len} ))" "${count}"
-    else
-        blink "${1}" "$(( 2024 * stone ))" "${count}"
-    fi
-}
-
 # depth stones...
-iterative() {
+blinkx() {
     local -a stones queue
     local i j len depth
     depth="${1}"
@@ -62,8 +43,8 @@ main() {
     local -a initial
 
     read -ra initial < "${1:-/dev/stdin}"
-    iterative "25" "${initial[@]}"
-    iterative "75" "${initial[@]}"
+    blinkx "25" "${initial[@]}"
+    blinkx "75" "${initial[@]}"
 }
 
 main "${@}"
