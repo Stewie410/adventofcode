0a004d81b5b87e484383065ad0bb57b98ba3fb54
Alex Paarfus <stewie410@gmail.com>
2025-12-07 10:44:34 -0500

    opt(bash): 2025-06: p1()/p2() as parsers only, do math in main()

--- bash/2025/06.sh
+++ bash/2025/06.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-# ref(lines), ref(opers)
+# ref(lines), ref(out)
 p2() {
     local -n data="${1}"
-    local -n ops="${2}"
+    local -n cols="${2}"
 
     local i len
     for i in "${data[@]}"; do
         (( ${#i} > len )) && len="${#i}"
     done
 
-    local -a cols
-    local j c char csv slice
-    for (( c = 0, i = 0; i < len; i++ )); do
+    local j char csv slice
+    for (( i = 0; i < len; i++ )); do
         slice=""
         for j in "${data[@]}"; do
             char="${j:i:1}"
@@ -21,30 +20,20 @@ p2() {
         if [[ "${slice}" == *[[:digit:]]* ]]; then
             csv+="${slice},"
         else
-            cols["${c}"]="${csv%,}"
+            cols+=( "${csv%,}" )
             csv=""
-            (( c++ ))
-            continue
         fi
     done
-    cols["${c}"]="${csv%,}"
-
-    local op prob total
-    for (( i = 0; i < ${#cols[@]}; i++ )); do
-        op="${ops[i]}"
-        prob="${cols[i]//,/${op}}"
-        (( total += prob ))
-    done
-
-    printf '%d\n' "${total}"
+    cols+=( "${csv%,}" )
 }
 
-# ref(lines), ref(opers)
+# ref(lines), ref(out)
 p1() {
     local -n data="${1}"
-    local -n ops="${2}"
+    # shellcheck disable=SC2178
+    local -n cols="${2}"
 
-    local -a cols t
+    local -a t
     local i j
     for i in "${data[@]}"; do
         read -ra t <<< "${i}"
@@ -53,16 +42,6 @@ p1() {
         done
     done
     cols=( "${cols[@]%,}" )
-
-    # this is absolutely *wild* that it works
-    local op prob total
-    for (( i = 0; i < ${#cols[@]}; i++ )); do
-        op="${ops[i]}"
-        prob="${cols[i]//,/${op}}"
-        (( total += prob ))
-    done
-
-    printf '%d\n' "${total:-0}"
 }
 
 main() {
@@ -72,8 +51,21 @@ main() {
     read -ra opers <<< "${lines[-1]}"
     unset 'lines[-1]'
 
-    p1 "lines" "opers"
-    p2 "lines" "opers"
+    local -a cols1 cols2
+    p1 "lines" "cols1"
+    p2 "lines" "cols2"
+
+    local i op prob p1 p2
+    for (( i = 0; i < ${#opers[@]}; i++ )); do
+        op="${opers[i]}"
+        prob="${cols1[i]//,/${op}}"
+        (( p1 += prob ))
+
+        prob="${cols2[i]//,/${op}}"
+        (( p2 += prob ))
+    done
+
+    printf '%d\n' "${p1}" "${p2}"
 }
 
 main "${@}"
